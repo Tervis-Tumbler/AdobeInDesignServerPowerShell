90dc12102dac9c168e969876392cd1c2a78edba2
Chris Magnuson <hourglasssand@gmail.com>
2019-03-11 10:17:59 -0400

    Add comment with link to track back escaping xml text

--- AdobeInDesignServerPowerShell.psm1
+++ AdobeInDesignServerPowerShell.psm1
@@ -219,6 +219,7 @@ function Invoke-InDesignServerRunScriptDirectlyWithInlineSOAP {
         [Parameter(Mandatory)]$InDesignServerInstance,
         [Parameter(Mandatory)]$ScriptText
     )
+    #https://stackoverflow.com/questions/1132494/string-escape-into-xml
     $Body = @"
 <?xml version="1.0" encoding="utf-8"?>
 <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
